bc152881fa0117f904046a19454d6b9b9c111d8c
gavin lazar suntop <gavin@gsuntop.com>
2017-09-18 12:13:47 -0700

    Revert "Expose homepage data on the API (#739)"
    This reverts commit 5402317644cba660d01b36a079d5c3cbfd9abc20.

--- network-api/app/networkapi/homepage/views.py
+++ network-api/app/networkapi/homepage/views.py
@@ -1,23 +1,3 @@
-from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from django.http import Http404
+# from django.shortcuts import render
 
-from networkapi.homepage.models import Homepage
-from networkapi.homepage.serializers import HomepageSerializer
-
-
-class HomepageView(APIView):
-    """
-    A view that permits a GET to receive the related models on the homepage
-    """
-    permission_classes = (AllowAny,)
-
-    def get(self, request, format=None):
-        homepage = Homepage.objects.last()
-
-        if homepage is None:
-            raise Http404('Homepage has not been created yet')
-
-        serializer = HomepageSerializer(homepage)
-        return Response(serializer.data)
+# Create your views here.
